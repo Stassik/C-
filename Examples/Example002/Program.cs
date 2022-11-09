d9ae66bf36ccc95a0b48ac20165b5b1addca7c8e
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-09 23:15:26 +0300

    add programm example002

--- Examples/Example002/Program.cs
+++ Examples/Example002/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Привет! Как тебя зовут?");
+string username = Console.ReadLine();
+Console.WriteLine("Очень приятно, "+ username);
