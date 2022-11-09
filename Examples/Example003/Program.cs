7a9d34cf83d9a19f852ac6aaf05d15a06436d2f1
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-10 00:38:17 +0300

    modified Example003

--- Examples/Example003/Program.cs
+++ Examples/Example003/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Здравствуйте! Пожалуйста представьтесь.");
+string username = Console.ReadLine();
+Console.WriteLine(username + ", чтобы произвести сложение, Вам необходимо ввести два числа");
+Console.WriteLine("Введите первое число.");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число.");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Сумма введенных чисел равна " + (firstNumber + secondNumber));
