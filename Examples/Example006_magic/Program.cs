784588d37a9213c7e47b7d693b383c5ae3207e13
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-11 00:03:59 +0300

    add program Example006_magic

--- Examples/Example006_magic/Program.cs
+++ Examples/Example006_magic/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear(); // Отчистка консоли
+
+
+// Зададим переменные (координаты трех точек)
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya); //Будем рисовать, аргументы (отступ от левого края(количество символов), отступ от верхнего края(количество строк))
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while (count < 100000) {
+    int what = new Random().Next(0, 3); //числа 0, 1, 2
+    if (what == 0) {
+        x = (x + xa)/2; // находим середину отрезка
+        y = (y + ya)/2;
+    };
+    if (what == 1) {
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    };
+    if (what == 2) {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    };
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+
+    count++ ;
+};
