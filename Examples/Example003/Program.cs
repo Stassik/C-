c441026a093c83e21924d2e068fe547d7f2ac75e
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-10 22:04:47 +0300

    add information to Example003

--- Examples/Example003/Program.cs
+++ Examples/Example003/Program.cs
@@ -1,8 +1,20 @@
-﻿Console.WriteLine("Здравствуйте! Пожалуйста представьтесь.");
+﻿// Приветствие
+Console.WriteLine("Здравствуйте! Пожалуйста представьтесь.");
 string username = Console.ReadLine();
+
+// Сложение двух чисел
 Console.WriteLine(username + ", чтобы произвести сложение, Вам необходимо ввести два числа");
 Console.WriteLine("Введите первое число.");
 int firstNumber = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число.");
 int secondNumber = int.Parse(Console.ReadLine());
 Console.WriteLine("Сумма введенных чисел равна " + (firstNumber + secondNumber));
+
+// Деление двух чисел
+
+Console.WriteLine(username + ", чтобы произвести деление, Вам необходимо ввести два числа");
+Console.WriteLine("Введите числитель.");
+double firstNumberDel = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите знаменатель.");
+double secondNumberDel = int.Parse(Console.ReadLine());
+Console.WriteLine("Частное чисел равна " + (firstNumberDel / secondNumberDel));
