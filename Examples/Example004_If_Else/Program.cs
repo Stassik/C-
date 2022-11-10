3ce16c4417f2a6a1999f32bcf9549c476f744138
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-10 22:45:56 +0300

    add program example004_If_Else

--- Examples/Example004_If_Else/Program.cs
+++ Examples/Example004_If_Else/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Если юзера зовут Сергей, то выведем на экран "Сергей, лучший преподаватель!". Если нет то - "Ааа, это всего лишь (имя юзера)".
+
+
+// 1. Сделаем запрос имени
+Console.Write("Здравствуйте! Пожалуйста, представьтесь: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "сергей") {
+   Console.WriteLine("Сергей, лучший преподаватель!"); 
+}
+else {
+    Console.WriteLine ("Ааа, это всего лишь " + username);
+};
