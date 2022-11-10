4beff4da2321ff9a2c8b25c05700dabaf2cb39f7
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-10 23:03:50 +0300

    add program example005

--- Examples/Example005/Program.cs
+++ Examples/Example005/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найдем наибольшее число
+
+// 1 Вариант
+
+int a = 7;
+int b = 9;
+int c = 3;
+int d = 11;
+int e = 4;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Наибольшее чило - "+ max);
