ebe4964e8396731bce3eac09ef20cc02baaf87b1
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-12 23:14:04 +0300

    modified program Task06

--- Seminars/Task06/Program.cs
+++ Seminars/Task06/Program.cs
@@ -3,3 +3,12 @@
 // 4 -> да
 // -3 -> нет
 // // 7 -> нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) {
+    Console.WriteLine("Чётное число.");
+} else {
+    Console.WriteLine("Нечётное число.");
+}
