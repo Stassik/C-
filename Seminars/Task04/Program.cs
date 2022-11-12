0853bef9ea7297883bba7c879bbaa65a5cde68c7
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-12 22:59:19 +0300

    modified Task04

--- Seminars/Task04/Program.cs
+++ Seminars/Task04/Program.cs
@@ -4,3 +4,42 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32 (Console.ReadLine());
+
+if ((number1 == number2) & (number2 == number3)) { //Если все числа равны
+    Console.WriteLine("Числа равны!");
+} else {
+
+    if (number1 > number2) {
+        if (number1 > number3) {
+            Console.WriteLine("Первое число является максимальным!");
+        } else if (number1 < number3){
+            Console.WriteLine("Третье число является максимальным!");
+        } else {
+            Console.WriteLine("Первое и третье числа являются максимальными!"); //Если первое и третье числа равны
+        }
+
+    } else if (number1 < number2) {
+        if (number2 > number3) {
+            Console.WriteLine("Второе число является максимальным!");
+        } else if (number2 < number3) {
+            Console.WriteLine("Третье число является максимальным!");
+        } else {
+            Console.WriteLine("Второе и третье числа являются максимальными!"); //Если второе и третье числа равны
+        }
+
+    } else {                                                       //Если первое и второе числа равны 
+        if (number1 > number3) {
+            Console.WriteLine("Первое и второе числа являются максимальными!");
+        } else {
+            Console.WriteLine("Третье число является максимальным!");
+        }
+    }
+}    
