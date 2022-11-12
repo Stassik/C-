63588ae3a0171be3a8ed9c14546b6dac70be30e9
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-12 23:24:53 +0300

    modified program Task08

--- Seminars/Task08/Program.cs
+++ Seminars/Task08/Program.cs
@@ -2,3 +2,14 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+while (number <= N) {
+    if (number % 2 == 0) {
+        Console.Write($"{number} ");
+    }
+    number++;
+}
