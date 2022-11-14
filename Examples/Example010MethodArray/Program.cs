bd4dbd1d94260db3d7eeaa2c6bc974935f729c74
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-14 23:55:52 +0300

    modified program Example010

--- Examples/Example010MethodArray/Program.cs
+++ Examples/Example010MethodArray/Program.cs
@@ -1,3 +1,16 @@
 ﻿int[] array = {1, 26, 30, 94, 65, 16, 77, 84};
 
 int n = array.Length;
+Console.Write("Введите искомое число: ");
+int find = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find) {
+        Console.WriteLine(index);
+        break;
+    };
+
+    index++;
+}
