5046a55afe766935b1e63632a6f161096d0122bd
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-14 23:16:24 +0300

    modified Program Example008

--- Examples/Example008IntroMethods/Program.cs
+++ Examples/Example008IntroMethods/Program.cs
@@ -5,7 +5,7 @@ int Max(int arg1, int arg2, int arg3)
     int result = arg1;
     if (arg2 > result) result = arg2;
     if (arg3 > result) result = arg3;
-    return result;
+    return result; 
 }
 
 int a1 = 21;
