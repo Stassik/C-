c854c59267a989abd2cbbd3aabcaf1af3a2a4f73
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-17 22:57:35 +0300

    modified program Task16

--- Seminars/Task16/Program.cs
+++ Seminars/Task16/Program.cs
@@ -6,3 +6,18 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool Square(int num1, int num2)
+{
+    if ((num1 == num2 * num2) || (num2 == num1 * num1)) return true;
+    return false;
+}
+
+bool result = Square(number1, number2);
+Console.WriteLine(result ? $"Одно число является квадратом второго" : $"Одно число НЕ является квадратом второго.");
