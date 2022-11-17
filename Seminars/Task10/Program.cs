e92756224a7a03f1828001f9b8d7339f5f2eeb8c
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-17 23:27:31 +0300

    modified program Task10

--- Seminars/Task10/Program.cs
+++ Seminars/Task10/Program.cs
@@ -7,3 +7,29 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberModification(int num)
+{
+    int result = (num / 10) % 10;
+    return result;
+}
+
+if (((number > 99)&&(number < 1000))) 
+    {
+        int numResult = NumberModification(number);
+        Console.WriteLine($"Вторая цифра трехзначного числа --> {numResult}"); 
+    }
+else if ((number > -1000)&&(number < -99)) 
+    {
+        int numResult = NumberModification(number) * (-1);
+        Console.WriteLine($"Вторая цифра трехзначного числа --> {numResult}"); 
+    }
+else 
+    {
+        Console.WriteLine("Неверное число!");
+    }
+    
+
