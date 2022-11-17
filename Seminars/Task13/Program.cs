a2b5edf9a1cf4bc3334e61600face202f96fba60
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-17 23:48:23 +0300

    modified program Task13

--- Seminars/Task13/Program.cs
+++ Seminars/Task13/Program.cs
@@ -6,3 +6,28 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FindThirdNumber(int num) {
+    while ((num > 999) || (num < -999))
+    {
+       num = num / 10;
+    }
+    int threeDigit = num % 10;
+    return threeDigit;
+}
+
+if ((number > 99))
+{
+    Console.WriteLine($"Третья цифра числа {number} --> {FindThirdNumber(number)}");
+}
+else if ((number < -99))
+{
+    Console.WriteLine($"Третья цифра числа {number} --> {FindThirdNumber(number) * (-1)}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
