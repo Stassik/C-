598142e935db8c9a1ea7c4df42cc4099faa0faaa
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-18 00:02:26 +0300

    modified program Task15

--- Seminars/Task15/Program.cs
+++ Seminars/Task15/Program.cs
@@ -5,3 +5,22 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Введите цифру от 1 до 7: ");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+bool DayWeek( int day) {
+    if (day >= 6) return true;
+    return false;
+}
+
+bool result = DayWeek(dayWeek);
+
+if ((dayWeek > 0) && (dayWeek <= 7)) 
+{
+    Console.WriteLine(result ? $"Выходной!" : "Рабочий!");
+}
+else {
+    Console.WriteLine("Неверное число.");
+}
+
