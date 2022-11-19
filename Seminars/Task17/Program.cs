04e5f0b30f7a2644d832cde029d69cc2d97f8398
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-19 23:17:11 +0300

    add task condition program Task17

--- Seminars/Task17/Program.cs
+++ Seminars/Task17/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая:
+// 1. принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+// 2. выдаёт номер четверти плоскости, в которой находится эта точка.
+
