03ddc020d6fbef0eace7e60dbff6c8cc6b5e3fce
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-21 23:27:49 +0300

    modified Task19

--- Seminars/Task19/Program.cs
+++ Seminars/Task19/Program.cs
@@ -6,3 +6,31 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Palindrome(int num)
+{
+    int num1 = num / 10000;
+    int num5 = num % 10;
+    int num2 = (num / 1000) % 10;
+    int num4 = (num / 10) % 10;
+
+    if ((num1 == num5) && (num4 == num2)) return true;
+    return false;
+
+}
+
+if ((number > 9999) && (number < 100000))
+{
+    bool result = Palindrome(number);
+    Console.WriteLine(result ? $"Число является палиндромом" : $"Число НЕ является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Введено неверное число.");
+}
+
+
+
