8585ef1f63442fad6387aa9749ff2de7ae47011b
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-21 23:03:14 +0300

    modified Task22

--- Seminars/Task22/Program.cs
+++ Seminars/Task22/Program.cs
@@ -4,3 +4,32 @@
 
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+string Square(int number)
+{
+    int count = 1;
+    string table ="";
+    while (count <= number)
+    {
+        table = table + $"{count}   {count*count}\n";
+        count++;
+    }
+    return table;
+}
+
+Console.WriteLine(Square(num));
+
+
+
+// void Square (int number)
+// {
+//     for (int i = 1; i <= number; i++)
+//     {
+//         Console.WriteLine($"{i}   {i*i}");  
+//     }
+// }
+// Square(num);
