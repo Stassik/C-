1ab3aae281cb80de1a724480450f26a6ac6d2557
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-21 09:26:52 +0300

    modified Task20

--- Seminars/Task20/Program.cs
+++ Seminars/Task20/Program.cs
@@ -13,3 +13,28 @@
 
 // double n1 = 5.099985645;
 // double nRound = Math.Round(n1, 2, MidpointRounding.ToZero);
+
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("X: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Y:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Y:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double Distance (int xFirst, int yFirst, int xSecond, int ySecond)
+{
+    int distanceX = xFirst - xSecond;
+    int distanceY = yFirst - ySecond;
+    double hypotenuse = Math.Sqrt(distanceX * distanceX + distanceY * distanceY);
+    return hypotenuse;
+}
+double distance = Distance (x1, y1, x2, y2);
+double result = Math.Round(distance, 2, MidpointRounding.ToZero);
+Console.WriteLine(result);
