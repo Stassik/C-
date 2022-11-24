be7f1533e52eed10fd68e435615d73d9380062ad
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-24 22:30:01 +0300

    modified  program Task27

--- Seminars/Task27/Program.cs
+++ Seminars/Task27/Program.cs
@@ -5,3 +5,20 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int num)
+{
+    int sum = 0;
+    if (num < 0) num = num * (-1);
+    while (num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+int result = SumNumber(number);
+Console.WriteLine($"Сумма цифр числа {number} равна {result}");
