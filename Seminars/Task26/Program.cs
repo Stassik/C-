104ac070e30235a690cc9519bfc5279b35a5723d
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-24 15:00:26 +0300

    modified program Task26

--- Seminars/Task26/Program.cs
+++ Seminars/Task26/Program.cs
@@ -5,3 +5,19 @@
 // 456 -> 3
 // 78 -> 2
 // 89126 -> 5
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Length(int num)
+{   
+    int count = 0;
+    while(num != 0) {
+        num = num / 10;
+        count++;
+    }
+    return num == 0? 1 : count;
+}
+
+int result = Length(number);
+Console.WriteLine(result);
