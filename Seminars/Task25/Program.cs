c3110d67736a41531e899482e7099abde33a776b
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-24 16:31:52 +0300

    modified program Task25

--- Seminars/Task25/Program.cs
+++ Seminars/Task25/Program.cs
@@ -4,3 +4,21 @@
 
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число степени: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int num, int dgr)
+{
+    int cnst = num;
+    for(int i = 2; i <= dgr; i++)
+    {
+        num = num*cnst;
+    }
+    return num;
+}
+int result = Exponentiation(number, degree);
+Console.WriteLine((degree>0) ? result : "Введите положительное число степени.");
