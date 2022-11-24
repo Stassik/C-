8f1e3d5488707a6cd089d7fad1a7ce3c09621bc8
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-24 14:30:25 +0300

    modified program Task23

--- Seminars/Task24/Program.cs
+++ Seminars/Task24/Program.cs
@@ -5,3 +5,18 @@
 // 7 -> 28
 // 4 -> 10
 // 8 -> 36
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;    
+}
+int result = Sum(number);
+Console.Write($"{result}");
