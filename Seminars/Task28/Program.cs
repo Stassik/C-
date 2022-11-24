26026af76e2c72524bc30a5791dbb843b43e4ea9
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-24 15:24:06 +0300

    modified program Task28

--- Seminars/Task28/Program.cs
+++ Seminars/Task28/Program.cs
@@ -5,3 +5,18 @@
 // 4 -> 24
 // 5 -> 120
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Multiplication(int num)
+{
+    int multiple = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        multiple = multiple * i;
+    }
+    return multiple;
+}
+
+int result = Multiplication(number);
+Console.WriteLine((number > 0) ? result : $"Введите натуральное число");
