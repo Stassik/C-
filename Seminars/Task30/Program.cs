3e1c5e7ece5c7a17ee8465b77bcf83bea97910f5
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-24 16:10:54 +0300

    modified program Task30

--- Seminars/Task30/Program.cs
+++ Seminars/Task30/Program.cs
@@ -2,3 +2,20 @@
 // выводит массив из 8 элементов, заполненный
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
+Console.Write("Введите длинну массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[length];
+
+void PrintRandomArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+        Console.Write($"{arr[i]} ");
+    }
+}
+PrintRandomArray(array);
+
+
