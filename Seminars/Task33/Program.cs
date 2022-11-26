3e86f0fdd31507027ab9629b1151afbefcc1a467
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-26 23:37:13 +0300

    add new folder Task35

--- Seminars/Task33/Program.cs
+++ Seminars/Task33/Program.cs
@@ -3,3 +3,40 @@
 // определяет, присутствует ли заданное число в массиве.
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArray (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+bool FindNumber (int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;   
+    }   
+    return false;
+}
+Console.Write("Укажите искомое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(15, -9, 9);
+PrintArray(array);
+Console.WriteLine(FindNumber(array, number) ? "Да" : "Нет");
