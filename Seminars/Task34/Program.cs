6ba9a5a2220b5532b86c6fb872ea2cadaec6bad5
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-27 21:34:40 +0300

    modified program Task34

--- Seminars/Task34/Program.cs
+++ Seminars/Task34/Program.cs
@@ -2,3 +2,43 @@
 // 2. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+int[] CreateArray(int size)
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountingEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArray(sizeArray);
+int countEven = CountingEvenNumbers(arr);
+PrintArray(arr);
+Console.Write($"Колличество четных чисел - {countEven}.");
