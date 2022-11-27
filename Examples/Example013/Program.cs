d05502c98dee5aa010762912fa089e7821c5a635
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-11-27 23:12:09 +0300

    modified program example013

--- Examples/Example013/Program.cs
+++ Examples/Example013/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string[,] table = new string[2, 5];
+// table[1, 2] = "слово";
+
+// for (int row = 0; row < 2; row++)
+// {
+//     for (int column = 0; column < 5; column++)
+//     {
+//         Console.Write($"-{table[row, column]}");
+//     }
+//     Console.WriteLine();
+// }
+
+int[,] matrix = new int[4, 4];
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int column = 0; column < matrix.GetLength(1); column++)
+    {
+        Console.Write($"{matrix[row, column]} ");
+    }
+    Console.WriteLine();
+}
