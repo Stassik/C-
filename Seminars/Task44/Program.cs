52a088eaf5fc43c20dee43a20cbab11e5486551c
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-01 11:17:38 +0300

    add task condition of program Task44

--- Seminars/Task44/Program.cs
+++ Seminars/Task44/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Не используя рекурсию, выведите первые N чиселФибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1.
+
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
