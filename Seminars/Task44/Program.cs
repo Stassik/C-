c786de831527fb31657e8872d3b9d6a3f366307a
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-01 11:58:59 +0300

    add task condition of program Task44

--- Seminars/Task44/Program.cs
+++ Seminars/Task44/Program.cs
@@ -4,3 +4,32 @@
 // Если N = 5 -> 0 1 1 2 3
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateFibonacciArray (int numb)
+{
+   int[] arr = new int[numb];
+   arr[0] = 0;
+   arr[1] = 1;
+   for (int i = 2; i < arr.Length; i++)
+   {
+        arr[i] = arr[i - 1] + arr[i -2];   
+   }
+   return arr; 
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = CreateFibonacciArray(num);
+PrintArray(array);
