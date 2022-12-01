7d2a341edc5669c3c9c1af2a4e01e17904a942f3
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-01 17:25:08 +0300

    modified program Task43

--- Seminars/Task43/Program.cs
+++ Seminars/Task43/Program.cs
@@ -3,3 +3,31 @@
 // 2. значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите число b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double GetX(double bA, double bB, double kA, double kB)
+{
+    double x1 = (bB - bA) / (kA - kB);
+    return x1;
+}
+
+double GetY(double bA, double kA, double x1)
+{
+    double y1 = kA * x1 + bA;
+    return y1;
+}
+
+double x = Math.Round(GetX(b1, b2, k1, k2), 2, MidpointRounding.ToZero); // В принципе и без округления получается как в условии задачи
+double y = Math.Round(GetY(b1, k1, x), 2, MidpointRounding.ToZero);
+Console.WriteLine($"({x}; {y})");
