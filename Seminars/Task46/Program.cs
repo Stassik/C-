036dfa3e61719dad80f210432e2b8f29777c2215
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-03 23:06:31 +0300

    modified Task46

--- Seminars/Task46/Program.cs
+++ Seminars/Task46/Program.cs
@@ -4,3 +4,33 @@
 // 1 4 8 19
 // 5 -2 33 -2
 // 77 3 8 1
+
+int row = 3;
+int col = 4;
+
+int[,] CreateArray (int m, int n)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(-99, 100);
+        }
+    }
+    return arr;
+}
+void PrintArray (int[,] arr, int m, int n)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }        
+}
+int[,] array = CreateArray(row, col);
+PrintArray(array, row, col);
