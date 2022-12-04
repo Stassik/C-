748546a5db8822781cfe549e4da2664204e29944
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-04 14:40:05 +0300

    add task condition of programm Task48

--- Seminars/Task48/Program.cs
+++ Seminars/Task48/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
