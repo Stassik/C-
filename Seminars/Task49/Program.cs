f941412b3587b41b624a02ca24cb82588e5f7777
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-04 15:33:03 +0300

    modified program task49

--- Seminars/Task49/Program.cs
+++ Seminars/Task49/Program.cs
@@ -2,3 +2,48 @@
 // Найдите элементы, у которых оба индекса чётные, 
 // и замените эти элементы на их
 // квадраты.
+
+int row = 4;
+int col = 4;
+
+int[,] CreateArray(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return arr;
+}
+void PrintArray(int[,] arr, int m, int n)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] NewArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if ((i % 2 == 0) && (j % 2 == 0)) arr[i, j] *= arr[i, j];
+        }
+    }
+    return arr;
+}
+int[,] array = CreateArray(row, col);
+PrintArray(array, row, col);
+int[,] newArray = NewArray(array);
+Console.WriteLine();
+PrintArray(newArray, row, col);
