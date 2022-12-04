eb477227567a9f917f122d0957efc921cc03dbff
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-04 17:16:18 +0300

    modified program task52

--- Seminars/Task52/Program.cs
+++ Seminars/Task52/Program.cs
@@ -5,3 +5,49 @@
 // 8 4 2 4
 
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateArray(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return arr;
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+Console.Write("Введите количество строк в массиве: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+if (row > 0 && col > 0)
+{
+    int[,] array = CreateArray(row, col);
+    PrintArray(array);
+}
+else if (row == 0 || col == 0)
+{
+    Console.WriteLine("Одно из значений райно нулю. Введите целое число больше нуля!.");
+}
+else
+{
+    Console.WriteLine("Вы ввели отрицательное число. Введите целое число больше нуля!.");
+}
