02ce885d1df4b5543d4955b2655c4514195f205b
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-04 15:22:21 +0300

    modified program task48

--- Seminars/Task48/Program.cs
+++ Seminars/Task48/Program.cs
@@ -5,3 +5,38 @@
 // 0 1 2 3
 // 1 2 3 4
 // 2 3 4 5
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintArray (int[,] matrix, int row, int col)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }        
+}
+
+int[,] CreateMatrix (int row, int col)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i==0) matrix[i, j] = 0;
+            matrix[i, j] = j + i ;
+        }
+    }
+    return matrix;
+}
+
+int[,] array = CreateMatrix(m, n);
+PrintArray(array, m, n);
