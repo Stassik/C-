a26e1bc933da6afc60257f24c26523b766799eb3
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-06 23:12:45 +0300

    Modified program Task53

--- Seminars/Task53/Program.cs
+++ Seminars/Task53/Program.cs
@@ -1,3 +1,58 @@
 ﻿// Задайте двумерный массив. 
 // Напишите программу, которая поменяет местами первую и последнюю строку
 // массива.
+
+
+int[,] CreateMatrix(int row, int col)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ReposMatrix(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+
+
+    }
+    return matrix;
+}
+
+Console.Write("Введите количество строк: ");
+int rowNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int colNum = Convert.ToInt32(Console.ReadLine());
+
+if (rowNum < 0 || colNum < 0) Console.WriteLine("Вы ввели отрицательное число. Введите положительное целое число!.");
+else
+{
+    int[,] matrixArray = CreateMatrix(rowNum, colNum);
+    PrintArray(matrixArray);
+    Console.WriteLine();
+    int[,] reposMatrixArray = ReposMatrix(matrixArray);
+    PrintArray(reposMatrixArray);
+}
