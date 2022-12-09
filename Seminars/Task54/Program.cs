fb5d60b44f5d95c8057b38f7f5c1b5d13fd7e047
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-09 11:58:37 +0300

    modified program Task54

--- Seminars/Task54/Program.cs
+++ Seminars/Task54/Program.cs
@@ -8,3 +8,74 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] CreateMatrix(int row, int col)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < (matrix.GetLength(1) - k); k++)
+        {
+            int temp = 0;
+            for (int j = k; j < matrix.GetLength(1) - k; j++)
+            {
+                int min = matrix[i, matrix.GetLength(1) - k - 1];
+                int max = matrix[i, k];
+                if (matrix[i, j] > max)
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+                else if (matrix[i, j] < min)
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, matrix.GetLength(1) - k - 1];
+                    matrix[i, matrix.GetLength(1) - k - 1] = temp;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+Console.Write("Введите количество строк: ");
+int rowNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int colNum = Convert.ToInt32(Console.ReadLine());
+
+if (rowNum < 0 || colNum < 0) Console.WriteLine("Вы ввели отрицательное число. Введите положительное целое число!.");
+else
+{
+    int[,] matrixArray = CreateMatrix(rowNum, colNum);
+    PrintMatrix(matrixArray);
+    Console.WriteLine();
+
+    int[,] sortMatrixArray = SortMatrix(matrixArray);
+    PrintMatrix(sortMatrixArray);
+}
