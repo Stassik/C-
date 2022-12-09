119a90bb187933c70f7cb9141317f086f4d7f4cd
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-09 12:49:21 +0300

    modified program Task56

--- Seminars/Task56/Program.cs
+++ Seminars/Task56/Program.cs
@@ -9,3 +9,82 @@
 
 // Программа считает сумму элементов в каждой строке и 
 // выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrix(int row, int col)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindMinRowSum (int[,] matrix)
+{
+    int sumMax = 0;
+    int sumMin = 0;
+    int iMax = 0;
+    int iMin = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j]; 
+        }
+        if(i == 0)
+        {
+            sumMax = sum;
+            sumMin = sum; 
+            iMax = i;
+            iMin = i;
+        }
+        else
+        {
+            if(sum > sumMax)
+            {
+                sumMax = sum;
+                iMax = i;
+            }
+            if(sum < sumMin)
+            {
+                sumMin = sum; 
+                iMin = i;
+            }      
+        }
+    }
+    Console.WriteLine($"Наибольшая сумма в строке {iMax} - {sumMax}");
+    Console.WriteLine($"Наименьшая сумма в строке {iMin} - {sumMin}");
+}
+
+Console.Write("Введите количество строк: ");
+int rowNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int colNum = Convert.ToInt32(Console.ReadLine());
+
+if (rowNum < 0 || colNum < 0) Console.WriteLine("Вы ввели отрицательное число. Введите положительное целое число!.");
+else
+{
+    int[,] matrixArray = CreateMatrix(rowNum, colNum);
+    PrintMatrix(matrixArray);
+    Console.WriteLine();
+    FindMinRowSum(matrixArray);
+
+}
