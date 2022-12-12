2f1b113e9d0bfdeb76c244212e9c42686de3a914
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-12 23:03:44 +0300

    modified program Task67

--- Seminars/Task67/Program.cs
+++ Seminars/Task67/Program.cs
@@ -2,3 +2,24 @@
 // возвращать сумму его цифр.
 // 453 -> 12
 // 45 -> 9
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    int unit = 0;
+    while (num > 0)
+    {
+        unit = num % 10;
+        sum += unit;
+        num /= 10;
+        SumNumbers(num);
+    }
+
+    return sum;
+}
+
+int sumNumbers = SumNumbers(number);
+Console.Write(sumNumbers);
