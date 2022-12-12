5590ebb589cdff4f8c22fc072281fc641e1d8580
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-12 22:48:12 +0300

    add task condition of programm Task67

--- Seminars/Task67/Program.cs
+++ Seminars/Task67/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
