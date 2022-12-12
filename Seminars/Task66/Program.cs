cc0de90f80aa17ee0d433a91db685dc24cf610e9
Anastasiya Evteeva <nastase4ka@mail.ru>
2022-12-13 00:46:21 +0300

    modified program Task66

--- Seminars/Task66/Program.cs
+++ Seminars/Task66/Program.cs
@@ -4,3 +4,21 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int numM, int numN)
+{
+    if (numM > numN) return numM + SumNumbers(numM - 1, numN);
+    if (numN > numM) return numM + SumNumbers(numM + 1, numN);
+    return numM;
+}
+
+if (numberM <= 0 || numberN <= 0) Console.WriteLine(" Введите натуральные числа!");
+else
+{
+    Console.WriteLine(SumNumbers(numberM, numberN));
+}
